81d581f1ffce3640f77a221c3eeacf65ad572eb1
吴子鑫 <1123116007@qq.com>
2018-03-21 05:00:20 -0500

    applocal

--- app/components/goodslist/goodslist.component.ts
+++ app/components/goodslist/goodslist.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {GetdataService} from '../../services/getdata.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router'; 
+
 @Component({
   selector: 'app-goodslist',
   templateUrl: './goodslist.component.html',
@@ -7,14 +10,22 @@ import {GetdataService} from '../../services/getdata.service';
 })
 export class GoodslistComponent implements OnInit {
 
-  constructor(public getdata:GetdataService) { 
-
-  }
-  num:number;
-  @Input() list:Array<any>;
+  constructor(public router:ActivatedRoute) { }
+  num;
   ngOnInit() {
-    this.getdata.addData(20);
-    this.getdata.addData(30);
-    console.log(this.getdata.list);    
+    this.num=this.router.snapshot.params;
+    console.log(this.router.snapshot.params);//组件之间值的传递
   }
+  // constructor(public router:Router,@optional()){}
+  idx:number[]=[1,2,3,4,5];
+  // goDetail(idx){
+  //   this.router.navigate(['/gooddetail',idx]);
+  // }
+  // num:number;
+  @Input() list:Array<any>;
+  // ngOnInit() {
+  //   this.getdata.addData(20);
+  //   this.getdata.addData(30);
+  //   console.log(this.getdata.list);    
+  // }
 }
